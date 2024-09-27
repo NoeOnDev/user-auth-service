d0ac104b0e75df36ce284e6ebe7c643bbf52dc6f
NoeOnDev <alxg5516@gmail.com>
2024-09-27 17:51:51 -0600

    Añadir configuración de archivos estáticos y ruta de inicio

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
+import path from "path";
 import { env } from "./_config/env.config";
 import { connectWithRetry } from "./_utils/dbConnection";
 
@@ -12,6 +13,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -19,6 +22,10 @@ const limiter = rateLimit({
 });
 
 app.use(limiter);
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to the Microservice users API 🚀");
+});
 
 connectWithRetry(10, 10000, () => {
   app.listen(port, () => {
