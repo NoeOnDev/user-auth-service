eaa41c3cdc60492f311b75cf34376b673bf68b2b
NoeOnDev <alxg5516@gmail.com>
2024-10-02 10:54:16 -0600

    Refactorizar clase User para eliminar objetos de valor innecesarios

--- src/users/domain/entity/User.ts
+++ src/users/domain/entity/User.ts
@@ -1,16 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
-import { UserEmail } from "../value-objects/UserEmail";
-import { UserPassword } from "../value-objects/UserPassword";
-import { UserPhone } from "../value-objects/UserPhone";
 
 export class User {
   constructor(
     public id: number,
     public uuid: string = uuidv4(),
     public name: string,
-    public email: UserEmail,
-    public password: UserPassword,
-    public phone: UserPhone,
+    public email: string,
+    public phone: string,
+    public password: string,
     public isEmailVerified: boolean,
     public isPhoneVerified: boolean,
     public createdAt: Date,
