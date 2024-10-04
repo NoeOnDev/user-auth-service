c9ba6fb42ff09028b2ed3a254a35e774ff72608b
NoeOnDev <alxg5516@gmail.com>
2024-10-04 03:24:29 -0600

    Refactorizar límite de solicitudes por IP a 50 en lugar de 100

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 50,
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
